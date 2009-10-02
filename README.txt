8b0b56e7b6fa9fd3e7e534077a4071bd3e03f49f
Marak <marak.squires@gmail.com>
2009-10-02 07:58:50 -0400

    refactored to use new selector

--- README.txt
+++ README.txt
@@ -1,5 +1,22 @@
- /* insert MIT liscense here */
-/* jquery.route.js written by Marak Squires 06/09 */
-/* Thanks goes out to Charlie Robbins for his guidance on Inversion of Control
-   and of course John Resig for making jQuery and being my Javascript hero */
-   
+/*********************************************************************** 
+
+  jquery.route.js enables you to create "routes" based on a unique URI
+  routes are states that may have events bound to them  routes can be 
+  triggered anywhere by calling route('/foo').run()
+  this is a javascript implementation of Inversion of Control
+
+  *** http://en.wikipedia.org/wiki/Inversion_of_control ***
+     	    *** "Don't call us, we'll call you" ***
+
+  USAGE:
+	
+  route('#/account').bind(customMethod);
+  route('#/account').bind(customMethod2);			
+			
+  route('#/websites').bind(customMethod);
+  route('#/websites').bind(customMethod2);			
+			
+  route('#/account').run();
+  route('#/websites').run();
+			
+*************************************************************************/
--- jquery.route.js
+++ jquery.route.js
@@ -1,133 +1,78 @@
-/* 
- * this is a javascript implementation of Inversion of Control
- * http://en.wikipedia.org/wiki/Hollywood_Principle
- * "Don't call us, we'll call you"
+/*
+Copyright (c) 2009 Marak Squires
+ 
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+ 
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 
- */
-var route = {};
-var routes = new Array();
-/* this method will register a location.hash path with an element and an event
- * whenever that event is triggered on that element, all accepting states
- * that are also bound to the location.hash path will fire */
-route.startState = function(el,path,eventTrigger){
-	/* TODO: strip out hostnames such as ("http://","www.","domainname",".com",".net","/") */
-	/* you could also do REGEX replacements here */
-	//path = path.replace( new RegExp( 'searchStringWithRegex', 'gi' ), 'replacementText');
+*/
+
+
+/*********************************************************************** 
+
+  jquery.route.js enables you to create "routes" based on a unique URI
+  routes are states that may have events bound to them  routes can be 
+  triggered anywhere by calling route('/foo').run()
+  this is a javascript implementation of Inversion of Control
+
+  *** http://en.wikipedia.org/wiki/Inversion_of_control ***
+     	    *** "Don't call us, we'll call you" ***
+
+  USAGE:
 	
-	/* register path and eventTrigger for element */
-	$(el).data('route', { 
-		path: path,
-		eventTrigger: eventTrigger
-	});
+  route('#/account').bind(customMethod);
+  route('#/account').bind(customMethod2);			
+			
+  route('#/websites').bind(customMethod);
+  route('#/websites').bind(customMethod2);			
+			
+  route('#/account').run();
+  route('#/websites').run();
+			
+*************************************************************************/
 
-	/* here is where we invert control of the method */
-	switch(eventTrigger)
-		{
-			/* we could add custom eventTriggers here */
-			/* bind() will work well for events such as "click" */
-			default:
-				$(el).bind(eventTrigger,function(){
-					route.enterState(path);
-				});
-			break;
-		}
-};
-
-/* this method will register a route to be executed after a 
- * start state has been initated. 
- */
-route.acceptState = function(el,path,type,returnFormat,uri){
-		var myRoute = {
-			type: type,
-			returnFormat: returnFormat,
-			uri: uri
-		};
-		var r = {};
-		r[path] = myRoute;
-		routes.push(r);
-		$('#newStates').html($.toJSON(routes));
-		$(el).data(path,myRoute);
-		return false;
-};
-
-/* this method will execute when a state is triggered */
-route.enterState = function(startState){
-
-//	console.log(startState);
-	/* use a custom data() selector to search for routes/states
-	   http://github.com/Marak/jquery.dataSelector.js/ 	*/
-	var myRoutes = $(':data('+startState+')');
-	
-	$('#currentState').html(startState);
-	$.each(myRoutes,function(i,e){
-		var myRoute = $(e).data(startState);
-			/* determine type of route we are working with */
-			switch(myRoute.type)
-			{
-				case 'LOCAL':
-				// we are going to retrieve local data
-				break;
-				case 'REMOTE': 
-				// we are going to get remote data
-					switch(myRoute.returnFormat)
-					{
-						case 'JSON': 
-							/* request JSON from REMOTE server */
-							/* TODO: hook jquery.template.js here */
-							//$(myRoute.targetOutput).html('JSON gets sent here and then microtemplated with jquery.template.js');
-						break;
-						case 'HTML': 
-							/* request HTML fragment from REMOTE server */
-							//$(e).html(('Do Get Request to ' + myRoute.uri) + ' and fill up with the response HTML fragment.')
-							$.get(myRoute.uri,function(data){
-								$(e).html(data);										 
-							});
-						break;
-						case 'XML': 
-						break;
-						case 'PLAINTEXT': 
-						break;
-						case 'WDDX': 
-						break;
-						case 'CSV': 
-						break;
-
-						default:
-						break;
-					}
-				break;
-				default:
-				break;
-			}
-	});
-
-	/* update browser's url bar */
-	location.hash = startState;
-	return false;
-}; 
-
-route.override = function(){
-	/* POST */
-		/* override form submits */
-			var myPostActions = $('form').each(function(i,el){
-				var myPostAction = $(el).attr('action');
-				myPostAction = '#' + myPostAction;
-				route.startState(el,myPostAction,'click');
-				$(el).attr('action',myPostAction);
-				//override form action with onsubmit handler
-				$(el).submit(function(e){
-					return false;
-				});
-			});
-	/* GET */
-		/* override links */
-			var myGetActions = $('a').each(function(i,el){
-				var myGetAction = $(el).attr('href');
-				myGetAction = '#' + myGetAction;
-				route.startState(el,myGetAction,'click');
-				//override "href" with onclick handler
-				$(el).click(function(e){
-					return false;
-				});
-			});
+var route=function(path){
+  return new route.fn.init(path);
 }
+route.fn = route.prototype = {
+  init: function(path) {
+	  this.path = path;
+	  return true;
+  },
+  bind: function(fn) {
+    var routes = $(document).data('routes') || {};
+	if(typeof routes[this.path] == 'undefined'){routes[this.path]={};}	
+    if(typeof routes[this.path].events == 'undefined'){routes[this.path].events=new Array();}
+	routes[this.path].events.push(fn);
+	$(document).data('routes', routes);
+ 	return 'bind successful';
+  },
+  run: function() {
+	/* routes stored globally using $(document).data() */  
+	var routes = $(document).data('routes') || {};
+	if(typeof routes[this.path] == 'undefined'){routes[this.path]={};}	
+    if(typeof routes[this.path].events == 'undefined'){routes[this.path].events=new Array();}
+	for(var i=0; i<routes[this.path].events.length; i++){
+	  routes[this.path].events[i]();  
+	}
+	return 'run successful'; 
+  }
+};
+route.fn.init.prototype = route.fn;
