f7d3d15e7c459a3868d060dd07933e45cb971a84
Marak <marak.squires@gmail.com>
2009-10-02 12:41:49 -0400

    created route() selector

--- jquery.route.js
+++ jquery.route.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009 Marak Squires
+/* Copyright (c) 2009 Marak Squires - www.maraksquires.com
  
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
