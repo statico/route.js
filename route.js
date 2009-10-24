180b43b7387fad042d8529bb1984ab8158543b57
Marak <marak.squires@gmail.com>
2009-10-24 05:46:46 -0400

    removed regex matching, added route acquisition

--- route.js
+++ route.js
@@ -47,16 +47,24 @@ OTHER DEALINGS IN THE SOFTWARE.
   route('#/account').run();
   route('#/websites').run();
 
-  REGEX MATCHING:
-
-  When calling route(path) you may specify a REGEX string for path.
-
-
   WHERE THE ROUTES AT? 
   
   All routes are stored globally in window['routes']
   console.log(window['routes']);
+
+  ROUTE ACQUISITION:
+
+  If an immediate match isn't found for route(path) then the route will bubble 
+  to the next parent route with each key appended as an arguments hash 
   
+  route('#/account/marak.squires').run();
+  
+  No match found for '#/account/marak.squires'
+  Attempting to match '#/account', injecting arguments {'marak.squires':{}}
+  Match found for '#/account', running customMethod and customMethod2.
+
+  Arguments are available as a global reference in windows.routes.args
+
   PROTIP: Use a Dispatcher!
 
   var Biggie={};
@@ -83,39 +91,70 @@ var route=function(path){
 route.fn = route.prototype = {
   init: function(path) {
     /* lazy init window['routes'] */
-    if(typeof window['routes'] == 'undefined'){window['routes']={};}
+    if(typeof window['routes'] == 'undefined'){window['routes']={};window['routes'].args={};}
     if(typeof path=='undefined'){return;}
-
-    /* wrap path as whole word match */
-    this.path='\b'+path+'\b';
-    this.events=new Array();
-
-    for(r in window['routes']){ /* iterate through all existing routes */
-      if(r.search(new RegExp(this.path, 'i'))==0){ /* search existing route based on path REGEX */
-
-        /* a match has been found, assign matched route to current route instance */
-        this.path=r;
-
-        /* lazy init window['routes'][path].events */
-        if(typeof window['routes'][this.path].events == 'undefined'){window['routes'][this.path].events=new Array();}
-        this.events=window['routes'][this.path].events;
-        return;
-      }
-    }
-
-    /* since no matches were found lazy init empty route based on path */		
-    window['routes'][this.path]={};
-    window['routes'][this.path].events=new Array();
+	this.path=path;	
+	return false;
   },
+
+  /* route(this.path).bind() */
   bind: function(fn) {
     if(typeof fn == 'function'){
-      window['routes'][this.path].events.push(fn);
+ 	  if(typeof window['routes'][this.path]=='undefined'){
+	    window['routes'][this.path]={};
+	  }
+
+ 	  if(typeof window['routes'][this.path].events=='undefined'){
+	    window['routes'][this.path].events=new Array();
+	  }
+ 	  
+	  window['routes'][this.path].events.push(fn);
     }
   },
-  run: function() {
-    for(var i=0; i<this.events.length; i++){
-      this.events[i]();  
-    }
+  
+  /* route(this.path).run() */
+  run: function(args) {
+    if(typeof window['routes'][this.path]=='undefined'){
+	  
+	  /* since no route was found, splice off last part of location.hash, add to args and retry with new partial route */
+	  	//console.log('no match found for ' + this.path);	
+		
+		/* define function args hash */
+		if(typeof args == 'undefined'){var args = {};} 
+		if(typeof args.path == 'undefined'){args.path = '';} 	
+	 
+		/* split path into array based on "/" */
+		var pathArgs = this.path.split('/');
+		
+		/* terminating condition, stop parsing path when you reach end of string */
+		if(pathArgs.length<=1){
+			return false
+		}
+
+		/* remove the last element of the array and append it to args */
+		args.path = args.path + '/' + pathArgs[pathArgs.length-1];
+		pathArgs = pathArgs.splice(0,pathArgs.length-1);					
+		
+		/* re-assembled path after we have removed last key */
+		var p = "";
+		for(prop in pathArgs){
+			p=p+pathArgs[prop]+'/';
+		}
+		
+		/* remove trailing '/' */
+		p=p.substring(0,p.length-1);
+		
+		/* run new route with previous keys appeneded as args */
+		route(p).run(args);
+		return false;
+	}
+	else{
+			//console.log('match found for ' + this.path);
+		window['routes'].args = args;
+		for(var i=0; i<window['routes'][this.path].events.length; i++){
+		  window['routes'][this.path].events[i]();  
+		}
+	}
   },
   events: new Array()
 };
